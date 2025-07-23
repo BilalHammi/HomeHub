057f15e96923443c810b3eb024ebde60b20d5fdc
Bilal Hammi <bilal.hammi@hva.nl>
2025-07-23 16:06:21 +0200

    created application environment. + begin with register navbar.

---
+++ frontend/src/app/register/register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  imports: [],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  logoHome: string = '../../assets/logo.png';
+}
