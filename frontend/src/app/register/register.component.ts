0dd12a37b8338c4bfb43e7acb14c25912d8419ea
Bilal Hammi <bilal.hammi@hva.nl>
2025-08-10 15:12:59 +0200

    register form added functions to form check.

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,12 +1,89 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RegisterCheck } from '../services/register.service';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
+//
 
 @Component({
   selector: 'app-register',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  phoneWarning = '';
+  passwordWarning = '';
+  firstnameWarning = '';
+  lastnameWarning = '';
+  emailWarning = '';
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private registerService: RegisterCheck) {
+    this.registerForm = this.fb.group({
+      firstname: ['', [Validators.required, Validators.pattern(/^[^0-9]*$/)]],
+      lastname: ['', [Validators.required, Validators.pattern(/^[^0-9]*$/)]],
+      email: ['', [Validators.required, Validators.email, Validators.pattern(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/)]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.pattern(/^(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).+$/),
+        ],
+      ],
+      phonenumber: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(10),
+          Validators.minLength(10),
+          Validators.pattern('^[0-9]*$'),
+        ],
+      ],
+    });
+  }
+
+  onSubmit() {
+    
+    this.registerService.trimFunction(this.registerForm);
+
+    if (this.registerForm.valid) {
+      console.log('Form is valid!', this.registerForm.value);
+    } else {
+      console.log('Form is invalid!');
+
+      Object.keys(this.registerForm.controls).forEach((key) => {
+        const control = this.registerForm.get(key);
+        if (key === 'phonenumber') {
+          this.registerService.PhonenumberCheck(control?.errors ?? null);
+          this.phoneWarning = this.registerService.phoneWarning;
+        } else if (key === 'password') {
+          this.registerService.PasswordCheck(control?.errors ?? null);
+          this.passwordWarning = this.registerService.passwordWarning;
+        } else if (key === 'email') {
+          this.registerService.EmailCheck(control?.errors ?? null);
+          this.emailWarning = this.registerService.emailWarning;
+        } else if (key === 'firstname' || key === 'lastname') {
+          console.log(control?.errors);
+
+          this.registerService.NameChecker(control?.errors ?? null, key);
+          this.firstnameWarning = this.registerService.firstnameWarning;
+          this.lastnameWarning = this.registerService.lastnameWarning;
+        }
+      });
+    }
+  }
 }
---
+++ frontend/src/app/services/register.service.ts
@@ -0,0 +1,100 @@
+import { Injectable } from '@angular/core';
+import { ValidationErrors, FormGroup } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RegisterCheck {
+  form!: FormGroup;
+  registerForm!: FormGroup;
+  firstnameWarning = '';
+  lastnameWarning = '';
+  emailWarning = '';
+  phoneWarning = '';
+  passwordWarning = '';
+
+  public NameChecker(errors: ValidationErrors | null, key: string) {
+    console.log(key);
+    
+    if (key === 'firstname') {
+      this.firstnameWarning = '';
+
+      if (errors) {
+        console.log(errors);
+        
+        if (errors['required']) {
+          this.firstnameWarning = 'Input cannot be empty!';
+        } else if(errors['pattern']) {
+          console.log("Ik ben un de oattern");
+          
+          this.firstnameWarning = 'Input cannot have numbers!';
+        }
+      }
+    } else {
+      this.lastnameWarning = '';
+
+      if (errors) {
+        if (errors['required']) {
+          this.lastnameWarning = 'Input cannot be empty!';
+        }  else if(errors['pattern']) {
+          this.lastnameWarning = 'Input cannot have numbers!';
+        }
+      }
+    }
+  }
+
+  public EmailCheck(errors: ValidationErrors | null) {
+    this.emailWarning = '';
+
+    if (errors) {
+      if (errors['required']) {
+        this.emailWarning = 'Email cannot be empty!';
+      } else if (errors['pattern'] && errors['email']) {
+        this.emailWarning = `Email is incorrect`;
+      }
+    }
+  }
+
+  public PhonenumberCheck(errors: ValidationErrors | null) {
+    this.phoneWarning = '';
+
+    if (errors) {
+      console.log('Errors object:', errors);
+
+      if (errors['required']) {
+        this.phoneWarning = 'Phonenumber cannot be empty!';
+      } else if (errors['pattern']) {
+        this.phoneWarning = `Only numbers are allowed!`;
+      } else if (errors['maxlength']) {
+        this.phoneWarning = `Phonenumber cannot exceed ${errors['maxlength'].requiredLength} digits! (currently ${errors['maxlength'].actualLength})`;
+      } else if (errors['minlength']) {
+        this.phoneWarning = `Phonenumber must be at least ${errors['minlength'].requiredLength} digits! (currently ${errors['minlength'].actualLength})`
+      }
+    }
+  }
+
+  public PasswordCheck(errors: ValidationErrors | null) {
+    this.passwordWarning = '';
+
+    if (errors) {
+      if (errors['required']) {
+        this.passwordWarning = 'Password cannot be empty!';
+      } else if (errors['minlength']) {
+        this.passwordWarning = `Password must be at least ${errors['minlength'].requiredLength} digits! (currently ${errors['minlength'].actualLength})`;
+      } else if (errors['pattern']) {
+        this.passwordWarning = `There must be atleast 1 special character!`;
+      }
+    }
+  }
+
+  public trimFunction(trimValue: FormGroup) {
+    console.log(trimValue);
+    Object.keys(trimValue.controls).forEach((key) => {
+      const value = trimValue.get(key)?.value || '';
+      trimValue
+        .get(key)
+        ?.setValue(value.trim(), { emitEvent: false });
+      console.log(value.length);
+    });
+  }
+}
